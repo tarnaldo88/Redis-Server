88a0dc45cee2ff1a1f5b924d1e1b5d30c2f6eceb
Arnaldo <tarnaldo88@gmail.com>
2025-08-22 15:44:23 -0700

    Implementing the run function

--- src/RedisServer.cpp
+++ src/RedisServer.cpp
@@ -9,6 +9,15 @@ RedisServer:: RedisServer(int port) : port(port), server_socket(-1), running(tru
 
 void RedisServer::run()
 {
+    server_socket = socket(AF_INET, SOCK_STREAM, 0);
+
+    if(server_socket < 0) {
+        std::cerr << "Error Creating Server Socket. \n";
+        return;        
+    }
+
+    int opt = 1;
+    setsockopt(server_socket, SOL_SOCKET, SO_REUSEADDR, &opt, sizeof(opt));
 }
 
 void RedisServer::shutdown()
