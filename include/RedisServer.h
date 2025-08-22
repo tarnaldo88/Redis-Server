5e37733b40e744f9ebd1d6d400fddf95336bd9f0
tarnaldo88 <tarnaldo88@gmail.com>
2025-08-22 15:13:18 -0700

    adding run and shutdown function skeleton

--- include/RedisServer.h
+++ include/RedisServer.h
@@ -1,11 +1,19 @@
+#include <atomic>
+#include <string>
+
 #ifndef REDIS_SERVER_H
 #define REDIS_SERVER_H
 
 class RedisServer{
 public:
     RedisServer(int port);
+    void run();
+    void shutdown();
+
 private:
     int port;
+    int server_socket;
+    std::atomic<bool> running;
 };
 
 #endif
--- src/RedisServer.cpp
+++ src/RedisServer.cpp
@@ -1,4 +1,12 @@
 #include "./include/RedisServer.h"
 
 
-RedisServer:: RedisServer(int port) : port(port) {}
+RedisServer:: RedisServer(int port) : port(port), server_socket(-1), running(true) {}
+
+void RedisServer::run()
+{
+}
+
+void RedisServer::shutdown()
+{
+}
